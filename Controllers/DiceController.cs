4c6a85ee8312e92490e191716c51d5fc39401e01
Josh Mann <jlmann72@gmail.com>
2020-09-02 11:18:53 -0400

    Dice roller works

--- Controllers/DiceController.cs
+++ Controllers/DiceController.cs
@@ -11,5 +11,15 @@ namespace BasicAPI.Controllers
     [ApiController]
     public class DiceController : ControllerBase
     {
+        // Inside the ( ) goes a string. Inside the string is any additional parts of the url
+        //  past /api/dice that we want. In this case we trite {sides} to say take whatever the user
+        // Provides after the last / parse it as an integer 
+        [HttpGet("{sides}")]
+        public int Roll(int sides)
+        {
+            var randomNumberGenerator = new Random();
+            var roll = randomNumberGenerator.Next(sides) + 1;
+            return roll;
+        }
     }
 }
